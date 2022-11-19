c6960ba588372de7b662d68419698bcc64ecdc83
Christian Zagazeta <christianzagazeta@icloud.com>
2022-11-18 21:14:08 -0500

    Added EndAuction method

--- Classes/Auction.cs
+++ Classes/Auction.cs
@@ -9,15 +9,21 @@ namespace SilentAuctionConsole.Classes
     internal class Auction
     {
         public string Name { get; set; }
-        public List<Item> ItemList { get; set; }
+        public List<Item> ItemList { get; set; } = new();
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public bool IsLive { get; set; } = false;
 
-        public Auction(DateTime startdate, DateTime enddate)
+        public Auction(string name, DateTime startdate, DateTime enddate)
         {
+            Name = name;
             StartDate = startdate;
             EndDate = enddate;
+        }
+
+        public void EndAuction()
+        {
+            IsLive = false;
         }
     }
 }
