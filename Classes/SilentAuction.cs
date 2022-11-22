6dd846359c853423b77d56346527621206e4c28c
Christian Zagazeta <christianzagazeta@icloud.com>
2022-11-21 20:39:59 -0500

    Added SilentAuction class

---
+++ Classes/SilentAuction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentAuctionConsole.Classes
+{
+    internal class SilentAuction
+    {
+        List<Auction> Auctions { get; set; } = new();
+
+
+        public SilentAuction()
+        {
+
+        }
+
+    }
+}
