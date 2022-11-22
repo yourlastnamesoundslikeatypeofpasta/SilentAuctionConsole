8aa59a626ed21f1b9e0c3d17cf983f399f54c02c
Christian Zagazeta <christianzagazeta@icloud.com>
2022-11-21 22:30:38 -0500

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using SilentAuctionConsole.Classes;
+
+
+main();
+
+void main()
+{
+    SilentAuction silentauction = new();
+    bool login = silentauction.Login();
+
+    Console.WriteLine("All Users: ");
+    foreach (User user in silentauction.Users)
+    {
+        Console.WriteLine($"User ID: {user.ID}, User username: {user.Username}");
+    }
+    if (login)
+    {
+        Console.WriteLine($"Current Logged in user: {silentauction.LoggedInUser.Username}");
+    }
+    else
+    {
+        Console.WriteLine("No logged in user");
+    }
+
+
+}
+
+
+
