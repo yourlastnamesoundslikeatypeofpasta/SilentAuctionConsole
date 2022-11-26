6584ba9f0755ba110897bdd2227aaa2f5da2a39c
Christian Zagazeta <christianzagazeta@icloud.com>
2022-11-25 22:25:30 -0500

    Removed methods: AddUser RemoveUser CreateAuction StartAuction EndAuction

--- Classes/User.cs
+++ Classes/User.cs
@@ -21,6 +21,16 @@ namespace SilentAuctionConsole.Classes
             Username = username;
             Password = password;
         }
+
+        public void ChangePassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+
+        public void ChangeUsername(string newUsername)
+        {
+            Username = newUsername;
+        }
     }
 
     public class Admin : User
@@ -28,37 +38,6 @@ namespace SilentAuctionConsole.Classes
         public Admin(int id, string username, string password) : base(id, username, password)
         {
 
-        }
-
-
-        public User AddUser(List<User> users, string username, string password)
-        {
-            int id = users.Count + 1;
-            User newUser = new(id, username, password);
-            return newUser;
-        }
-
-        public List<User> RemoveUser(List<User> users, User user)
-        {
-            users.Remove(user);
-            return users;
-        }
-
-        public Auction CreateAuction(string auctionname, DateTime startdate, DateTime enddate)
-        {
-            Auction newAuction = new Auction(auctionname, startdate, enddate);
-            return newAuction;
-        }
-
-        public Auction StartAuction(Auction auction)
-        {
-            auction.StartAuction();
-            return auction;
-        }
-        public Auction EndAuction(Auction auction)
-        {
-            auction.EndAuction();
-            return auction;
         }
     }
 
